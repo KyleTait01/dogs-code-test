9f5a14e7b1aa759134446302a1bed8dfea1cbc80
KyleTait01 <taitk58@gmail.com>
2025-07-29 14:54:39 +0100

    Initial commit - setup

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use("/dist", express.static(__dirname + "/dist"));
+app.use(express.urlencoded());
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+
+// home page
+app.get("/", async (req, res) => {
+    res.render('dogs', {  });
+});
